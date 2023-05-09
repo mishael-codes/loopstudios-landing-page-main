6919ab8d4eba59fee79d1d5f1938612f32c46c12
mishael-codes <enyimishael22@gmail.com>
2023-05-09 01:56:23 +0100

    reduced the transition duration for the ham menu

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let close = document.getElementById("close");
 
 open.addEventListener("click", () => {
   let navlist = document.getElementById("nav-list");
-  navlist.style.transitionDuration = ".7s";
+  navlist.style.transitionDuration = ".5s";
   navlist.style.marginTop = "0px";
   open.style.display = "none";
   close.style.display = "block";
@@ -11,7 +11,7 @@ open.addEventListener("click", () => {
 
 close.addEventListener("click", () => {
   let navlist = document.getElementById("nav-list");
-  navlist.style.transitionDuration = "4000ms";
+  navlist.style.transitionDuration = ".5s";
   navlist.style.marginTop = "-2000px";
   close.style.display = "none";
   open.style.display = "block";
